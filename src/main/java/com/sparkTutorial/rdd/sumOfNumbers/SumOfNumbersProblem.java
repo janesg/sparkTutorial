941caf1f210c1317027e93bd543cd73ce9ffec28
Gary Janes <gary_janes@hotmail.com>
2018-06-06 16:51:13 +0100

    Simple RDD example for summing prime numbers

--- src/main/java/com/sparkTutorial/rdd/sumOfNumbers/SumOfNumbersProblem.java
+++ src/main/java/com/sparkTutorial/rdd/sumOfNumbers/SumOfNumbersProblem.java
@@ -1,5 +1,15 @@
 package com.sparkTutorial.rdd.sumOfNumbers;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
 public class SumOfNumbersProblem {
 
     public static void main(String[] args) throws Exception {
@@ -9,5 +19,18 @@ public class SumOfNumbersProblem {
 
            Each row of the input file contains 10 prime numbers separated by spaces.
          */
+
+        Logger.getLogger("org").setLevel(Level.ERROR);
+        SparkConf conf = new SparkConf().setAppName("sumNumbers").setMaster("local[2]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<Integer> primeNums =
+                sc.textFile("in/prime_nums.text")
+                        .flatMap(line -> Arrays.asList(line.split("\t")).iterator())
+                        .map(numStr -> Integer.valueOf(numStr.trim()));
+
+        Integer sum = primeNums.reduce((x, y) -> x + y);
+
+        System.out.println("sum is : " + sum);
     }
 }
