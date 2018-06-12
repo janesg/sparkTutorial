f2fd89db253b66d746828e9ff711abc479ef9030
Gary Janes <gary_janes@hotmail.com>
2018-06-12 14:17:49 +0100

    Pair RDD example for groupByKey

--- src/main/java/com/sparkTutorial/pairRdd/groupbykey/AirportsByCountryProblem.java
+++ src/main/java/com/sparkTutorial/pairRdd/groupbykey/AirportsByCountryProblem.java
@@ -1,8 +1,21 @@
 package com.sparkTutorial.pairRdd.groupbykey;
 
+import com.sparkTutorial.rdd.commons.Utils;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 public class AirportsByCountryProblem {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         /* Create a Spark program to read the airport data from in/airports.text,
            output the the list of the names of the airports located in each country.
@@ -18,5 +31,37 @@ public class AirportsByCountryProblem {
            "Papua New Guinea",  ["Goroka", "Madang", ...]
            ...
          */
+
+        Logger.getLogger("org").setLevel(Level.ERROR);
+        SparkConf conf = new SparkConf().setAppName("airportsUpperCase").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> airports = sc.textFile("in/airports.text");
+        JavaPairRDD<String, Iterable<String>> airportNamesByCountry =
+                // Utils.COMMA_DELIMITER contains regex that ignores comma between double quotes
+                airports
+                        .mapToPair(airport -> {
+                            String[] fields = airport.split(Utils.COMMA_DELIMITER);
+                            return new Tuple2<>(fields[3], fields[1]);
+                        })
+                        .groupByKey();
+
+        // Display output sorted by key using natural order
+        // and with each list of airport names also sorted in natural order and with duplicates removed
+        for (Map.Entry<String, List<String>> entry :
+                airportNamesByCountry.collectAsMap().entrySet()
+                    .stream()
+                    .sorted(Comparator.comparing(Map.Entry::getKey))
+                    .collect(Collectors.toMap(
+                                entry -> entry.getKey(),
+                                entry -> StreamSupport.stream(entry.getValue().spliterator(), false)
+                                    .distinct()
+                                    .sorted()
+                                    .collect(Collectors.toList()),
+                                (e1, e2) -> e2,
+                                LinkedHashMap::new))
+                    .entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue().toString());
+        }
     }
 }
--- src/main/java/com/sparkTutorial/pairRdd/groupbykey/GroupByKeyVsReduceByKey.java
+++ src/main/java/com/sparkTutorial/pairRdd/groupbykey/GroupByKeyVsReduceByKey.java
@@ -21,6 +21,27 @@ public class GroupByKeyVsReduceByKey {
         List<String> words = Arrays.asList("one", "two", "two", "three", "three", "three");
         JavaPairRDD<String, Integer> wordsPairRdd = sc.parallelize(words).mapToPair(word -> new Tuple2<>(word, 1));
 
+        // Relationship between RDD, partition and node:
+        //  - a single RDD has one or more partitions scattered across multiple nodes,
+        //  - a single partition is processed on a single node,
+        //  - a single node can handle multiple partitions (with optimum 2-4 partitions per CPU according
+        //    to the official documentation)
+        //
+        // Since Spark supports pluggable resource management details of the distribution will depend on the
+        // one you use (Standalone, Yarn, Messos).
+        //
+        // ReduceByKey is much more efficient with larger data sets:
+        //  - Initially, entries are randomly distributed across partitions
+        //  - Within each partition, entries with a common key are reduced to a single entry
+        //    thus decreasing the overall number of entries
+        //  - This reduced number of entries is then shuffled across partitions such that entries
+        //    with a common key all exist on a single partition
+        //  - ReduceByKey is called a 2nd time on each partition to produce final result for each key
+        //
+        // Shuffling requires data to be moved between partitions over the network.
+        // It is the reduction in the number of entries (compared with GroupByKey) that makes
+        // ReduceByKey more efficient.
+
         List<Tuple2<String, Integer>> wordCountsWithReduceByKey = wordsPairRdd.reduceByKey((x, y) -> x + y).collect();
         System.out.println("wordCountsWithReduceByKey: " + wordCountsWithReduceByKey);
 
